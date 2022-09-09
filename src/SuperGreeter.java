d3a6676c48c923c15485ba1755f372c9e36625d2
CarlHarlang <carl856k@stud.kea.dk>
2022-09-09 13:47:06 +0200

    added greet

--- src/SuperGreeter.java
+++ src/SuperGreeter.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class SuperGreeter {
     public void start() {
         System.out.println("Det virker!");
@@ -5,5 +7,9 @@ public class SuperGreeter {
     public static void main(String[] args){
         SuperGreeter sg = new SuperGreeter();
         sg.start();
+        Scanner scan = new Scanner(System.in);
+        String name = scan.next();
+        System.out.println(" Hej " + name);
     }
+
 }
